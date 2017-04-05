889d3fef3bfdb76e35c49643fbc0f96f912212cf
Ryan Brandenburg <rybrande@microsoft.com>
2017-04-04 17:04:17 -0700

    Fix [TempData] for PageModel's

---
+++ test/Microsoft.AspNetCore.Mvc.ViewFeatures.Test/Internal/SaveTempDataFilter/SaveTempDataPropertyFilterTestBase.cs
@@ -0,0 +1,28 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System.Collections.Generic;
+using System.Reflection;
+
+namespace Microsoft.AspNetCore.Mvc.ViewFeatures.Internal
+{
+    public class SaveTempDataPropertyFilterTestBase
+    {
+        public static IList<TempDataProperty> BuildTempDataProperties<TSubject>()
+        {
+            var subjectType = typeof(TSubject);
+
+            var properties = subjectType.GetProperties(
+                BindingFlags.Public | BindingFlags.Instance | BindingFlags.DeclaredOnly);
+
+            var result = new List<TempDataProperty>();
+
+            foreach (var property in properties)
+            {
+                result.Add(new TempDataProperty(property, property.GetValue, property.SetValue));
+            }
+
+            return result;
+        }
+    }
+}
