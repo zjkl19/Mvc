20c63e105fb00f9ada178c8a7f891c81f53bf6c8
Doug Bunting <dougbu@microsoft.com>
2017-08-10 14:53:36 -0700

    Workaround #4821 and #6639

--- samples/MvcSandbox/Controllers/HomeController.cs
+++ samples/MvcSandbox/Controllers/HomeController.cs
@@ -2,6 +2,7 @@
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
 using Microsoft.AspNetCore.Mvc;
+using MvcSandbox.Models;
 
 namespace MvcSandbox.Controllers
 {
@@ -12,7 +13,12 @@ namespace MvcSandbox.Controllers
 
         public IActionResult Index()
         {
-            return View();
+            var model = new Container
+            {
+                MyClass = new MyClass(),
+            };
+
+            return View(model);
         }
     }
 }
