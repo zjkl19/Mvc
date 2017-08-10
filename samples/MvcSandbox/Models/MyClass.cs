127c56d00b0cd5cda36d6c7a3ccd5712c9c9a007
Doug Bunting <dougbu@microsoft.com>
2017-08-10 14:51:33 -0700

    Test out #6639 symptoms

---
+++ samples/MvcSandbox/Models/MyClass.cs
@@ -0,0 +1,14 @@
+﻿using System.Reflection.Emit;
+
+namespace MvcSandbox.Models
+{
+    public class MyClass
+    {
+        public PackingSize PackingSize { get; set; }
+    }
+
+    public class Container
+    {
+        public MyClass MyClass { get; set; }
+    }
+}
